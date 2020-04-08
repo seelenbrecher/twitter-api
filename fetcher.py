5b0776b39508fb36fd8866b5317c15b26eb16cf4
maulanaanab <maulanaanab@gmail.com>
2020-04-09 00:30:49 +0800

    update

--- fetcher.py
+++ fetcher.py
@@ -2,6 +2,7 @@ from tweepy import OAuthHandler, Cursor, API, TweepError
 
 from settings import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET
 import json
+import csv
 
 consumer_key = CONSUMER_KEY
 consumer_secret = CONSUMER_SECRET
@@ -13,17 +14,18 @@ class Fetcher():
         self.auth = OAuthHandler(consumer_key, consumer_secret)
 
         if with_access_token:
-            print('ya')
             self.auth.set_access_token(access_token, access_secret)
 
         self.api = API(self.auth)
 
     # search by q = keyword, geocode and language
-    def search(self, keyword, geocode):
+    def search(self, keyword, geocode, cnt):
         tweets = []
         tweet_id_flag = set()
-        # change items
-        cursor = Cursor(self.api.search, q=keyword, geocode=geocode, lang='id', tweet_mode='extended').items(10)
+        
+        # edit params here
+        cursor = Cursor(self.api.search, q=keyword, geocode=geocode, lang='id', tweet_mode='extended').items(cnt)
+
         while True:
             try:
                 status = cursor.next()
@@ -31,48 +33,36 @@ class Fetcher():
                     continue
                 tweet_id_flag.add(status.id)
 
-                tweets.append({
-                    'id': status.id,
-                    'full_text': status.full_text,
-                    'retweet_count': status.retweet_count,
-                })
-                print({
-                    'id': status.id,
-                    'full_text': status.full_text,
-                    'retweet_count': status.retweet_count,
-                })
-            except TweepError as err:
-                print(err)
+                tweets.append(status)
+                print(f'{len(tweets)} tweets collected')
+            except TweepError:
+                time.sleep(60 * 15)
                 continue
             except StopIteration:
                 break
         return tweets
 
     # search by user_name
-    def search_by_user(self, user_name):
+    def search_by_user(self, user_name, cnt):
         user = self.api.get_user(screen_name=user_name)
-        cursor = Cursor(self.api.user_timeline, user_id=user.id, tweet_mode='extended').items(1)
+
+        # edit params here
+        cursor = Cursor(self.api.user_timeline, user_id=user.id, tweet_mode='extended').items(cnt)
+
         tweets = []
         tweet_id_flag = set()
-        for i in range(0, 1):
+        while True:
             try:
                 status = cursor.next()
                 if status.id in tweet_id_flag:
                     continue
                 tweet_id_flag.add(status.id)
-                
-                print(status)
 
-                tweets.append({
-                    'id': status.id,
-                    'full_text': status.full_text,
-                    'retweet_count': status.retweet_count,
-                })
-                print({
-                    'id': status.id,
-                    'full_text': status.full_text,
-                    'retweet_count': status.retweet_count,
-                })
+                tweets.append(status)
+                print(f'{len(tweets)} tweets collected')
+            except TweepError:
+                time.sleep(60 * 15)
+                continue
             except StopIteration:
                 break
         
@@ -80,8 +70,25 @@ class Fetcher():
 
 def main():
     fetcher = Fetcher()
-    # fetcher.search('corona', '-6.227193,106.808098,10km')
-    fetcher.search_by_user('FiersaBesari')
+    tweets = []
+    # geocode format = <lat>,<long>,<radius>
+    #tweets = fetcher.search('corona', '-6.227193,106.808098,10km', 10)
+    # tweets = fetcher.search_by_user('_zahraul', 10)
+
+    with open('search_result.csv', mode='w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        #headers
+        csv_writer.writerow(['id', 'userid', 'full_text', 'retweet_count'])
+
+        for tweet in tweets:
+            csv_writer.writerow([
+                tweet.id,
+                tweet.user.id,
+                tweet.full_text,
+                tweet.retweet_count,
+            ])
+    
 
 if __name__ == '__main__':
     main()
